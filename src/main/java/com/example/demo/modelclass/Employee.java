86cb73b586a4745f6c27e7161b3ad97c043d4e88
surendranadhilla <surendranadhilla@gmail.com>
2024-09-05 13:15:16 -0500

    I have added Email Validation to employe class

--- src/main/java/com/example/demo/modelclass/Employee.java
+++ src/main/java/com/example/demo/modelclass/Employee.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 
 @Entity 
@@ -22,6 +23,7 @@ public class Employee {
 		private String name;
         @Column(name="ESalary")
 		private double salary;
+        @Email(message = "Email should be valid")
         @Column(name="EemailID")
 		private String emailId;
 		public int getEid() {
